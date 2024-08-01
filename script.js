73731c518a8a743fd38e4872ab337eabb45b12d5
gavelarz <gustavo.leite.1bmangal@gmail.com>
2024-08-01 18:52:06 +0000

    .

---
+++ script.js
@@ -0,0 +1,10 @@
+let formulario =
+    document.querySelector(".formulario");
+    //função para aparecer o form
+function aparecerformulario(){
+    formulario.style.left="50%";
+}
+//função para desaparecer o form
+function desaparecerformulario(){
+    formulario.style.left="-50%"
+}
